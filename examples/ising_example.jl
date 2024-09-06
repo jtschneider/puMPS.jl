208de359b678b56cd14801a7ed5a79fc5af75cee
Jan Schneider <jan.schneider@iff.csic.es>
2024-09-06 11:14:03 +0200

    last changes

--- examples/ising_example.jl
+++ examples/ising_example.jl
@@ -1,19 +1,28 @@
 using puMPS
+using LinearAlgebra
+using TensorKit, MPSKit
 
 D = 8
-N = 32
+N = 100
 
-M = rand_puMPState(ComplexF64, 2, D, N)
+# M = rand_puMPState(ComplexF64, 2, D, N)
+
+M2 = InfiniteMPS([Rep[ℤ₂](0 => 1, 1 => 1)], [Rep[ℤ₂](0 => D//2, 1 => D//2)])
+
+M = M2.AC
 
 H = ising_local_MPO(ComplexF64)
 
 vumps_opt!(M, H, 1e-6, maxitr=5) #Pre-optimization using DMRG-like method
-minimize_energy_local!(M, H, 1000, step=0.1)
+minimize_energy_local!(M, H, 1000, step=0.05)
+
+
+
 
 println("Computing excitations!")
 
-ks_tocompute = [-2,-1,0,1,2]
-num_states = [5,4,7,4,5]
+ks_tocompute = collect(-5:5)
+num_states = [6,5,4,7,4,5,6]
 
 ens, ks, exs = excitations!(M, ising_PBC_MPO_split(ComplexF64), ks_tocompute, num_states)
 
@@ -23,15 +32,14 @@ H2 = Hn_in_basis(M, ising_Hn_MPO_split(ComplexF64, 2, N), exs, ks_tocompute)
 ind1 = argmin(real.(ens))
 indT = argmax(abs.(H2[:,ind1]))
 
-en1 = ens[ind1]
-enT = ens[indT]
+en1 = real(ens[ind1])
+enT = real(ens[indT])
 
-fac = 2.0 / (enT-en1)
+fac = real(2.0 / (enT-en1))
 
 c = 2*abs2(H2[indT,ind1] * fac)
 @show real(c)
 
-using PyPlot
+using Plots
 
-plot(ks, real(ens), "o")
-show()
+p = Plots.scatter(ks, real(ens), label="momentum")
